f9cb9d55d0fa60601a55262df5671b57fd8f0fbf
Mehmet Kaan <mehmetkaan1996@gmail.com>
2025-05-12 22:01:32 +0300

    initial commit
    Initial Commit

---
+++ Marti-iOS-Case/Marti-iOS-Case/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Marti-iOS-Case
+//
+//  Created by Mehmet Kaan on 12.05.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
