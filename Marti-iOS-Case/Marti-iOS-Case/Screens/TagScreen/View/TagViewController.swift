4be2f847d466804823033da4931670029be35f5f
Mehmet Kaan <mehmetkaan1996@gmail.com>
2025-05-13 00:21:38 +0300

    Added TagScreen

---
+++ Marti-iOS-Case/Marti-iOS-Case/Screens/TagScreen/View/TagViewController.swift
@@ -0,0 +1,40 @@
+//
+//  TagViewController.swift
+//  Marti-iOS-Case
+//
+//  Created by Mehmet Kaan on 12.05.2025.
+//
+
+import UIKit
+import MapKit
+
+class TagViewController: UIViewController, MKMapViewDelegate {
+
+    @IBOutlet weak var tagMapView: MKMapView!
+    private let viewModel = TagScreenViewModel()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupMapView()
+        configureViewModelCallBack()
+    }
+    
+    private func setupMapView() {
+        tagMapView.delegate = self
+        tagMapView.showsUserLocation = true
+    }
+    
+    private func configureViewModelCallBack() {
+        viewModel.onLocationUpdate = { [weak self] location in
+            guard let self = self else { return }
+            let region = MKCoordinateRegion(center: location.coordinate,
+                                            latitudinalMeters: 500,
+                                            longitudinalMeters: 500)
+            self.tagMapView.setRegion(region, animated: true)
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = location.coordinate
+            annotation.title = "Konum \(self.viewModel.locations.count)"
+            self.tagMapView.addAnnotation(annotation)
+        }
+    }
+}
